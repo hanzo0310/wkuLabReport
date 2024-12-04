fef07309ada378721fe49b870fd67dc6d1a39c63
DESKTOP-2G9KVA4\user <yeomrihan04@gmail.com>
2024-12-05 04:53:16 +0900

    insert countStones function

--- Lab_O_mok/AI.h
+++ Lab_O_mok/AI.h
@@ -1,1 +1,21 @@
 #pragma once
+#include "var.h"
+
+int countStones(int board[SIZE][SIZE], int row, int col, int dRow, int dCol, int player) {
+    int count = 0;
+
+    for (int i = 1; i < 5; i++) {
+        int newRow = row + i * dRow;
+        int newCol = col + i * dCol;
+
+        if (newRow >= 0 && newRow < SIZE && newCol >= 0 && newCol < SIZE && board[newRow][newCol] == player) {
+            count++;
+        }
+        else {
+            break;
+        }
+    }
+    return count;
+}
+
+
