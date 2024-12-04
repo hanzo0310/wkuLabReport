e0a4eccd1c4c4ade2eb76ff39f4ad3d7be773a63
DESKTOP-2G9KVA4\user <yeomrihan04@gmail.com>
2024-12-05 04:37:21 +0900

    change to New Design
    Move Info Next to Stage

--- Lab_O_mok/screen.h
+++ Lab_O_mok/screen.h
@@ -2,17 +2,17 @@
 
 #include "var.h"
 
-void GotoXY(int x, int y) {
-    COORD pos;
-    pos.X = 2 * x;
-    pos.Y = y;
-
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
-}
-
 void setColor(int color) {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, color);
+}
+
+// 강조된 좌표가 포함된 바둑판 출력
+void GotoXY(int x, int y) {
+    COORD pos;
+    pos.X = 2 * x; // 가로 좌표는 2배로 확대하여 출력
+    pos.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
 // 강조된 좌표가 포함된 바둑판 출력
@@ -20,9 +20,16 @@ void printBoardWithHighlight(int board[SIZE][SIZE], int highlightPos[2], int tur
     system("cls"); // 화면 초기화
 
     // 바둑판 출력
+    GotoXY(0, 0);
+    printf("   ");
+    for (char c = 'A'; c < 'A' + SIZE; c++) {
+        printf("%c ", c); // 열 번호로 알파벳 출력
+    }
+    printf("\n");
+
     for (int i = 0; i < SIZE; i++) {
-        GotoXY(0, i);
-        printf("%2d ", i + 1); // 세로 번호 출력
+        GotoXY(0, i + 1);
+        printf("%2d ", i + 1); // 행 번호로 숫자 출력
         for (int j = 0; j < SIZE; j++) {
             if (i == highlightPos[0] && j == highlightPos[1]) {
                 SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 14); // 노란색
@@ -53,22 +60,41 @@ void printBoardWithHighlight(int board[SIZE][SIZE], int highlightPos[2], int tur
         }
     }
 
-    // 안내 문 출력 위치로 이동
+    // 안내문 출력 위치로 이동
     int infoX = SIZE + 3; // 바둑판 오른쪽의 x 좌표 (2배 확대 + 여백)
     int infoY = 0; // 바둑판 위쪽 y 좌표
 
-    // 안내 문 출력
+    // 안내문 출력
     GotoXY(infoX, infoY);
-    printf("오목 게임\n");
+    printf("오목 게임\n\n");
 
     GotoXY(infoX, infoY + 2);
     printf("파란색 : 선공\n");
 
     GotoXY(infoX, infoY + 3);
-    printf("빨간색 : 후공\n");
+    printf("빨간색 : 후공\n\n");
 
     GotoXY(infoX, infoY + 5);
-    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), turn == 0 ? 9 : 12);
+    printf("조작법:\n");
+
+    GotoXY(infoX, infoY + 6);
+    printf("  W / 윗 방향키 - 위로 이동\n");
+
+    GotoXY(infoX, infoY + 7);
+    printf("  A / 왼 방향키 - 왼쪽 이동\n");
+
+    GotoXY(infoX, infoY + 8);
+    printf("  S / 아랫 방향키 - 아래로 이동\n");
+
+    GotoXY(infoX, infoY + 9);
+    printf("  D / 오른 방향키 - 오른쪽 이동\n");
+
+    GotoXY(infoX, infoY + 10);
+    printf("  Space - 돌 놓기\n\n");
+
+    // 현재 턴 출력
+    GotoXY(infoX, infoY + 12);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), turn == 0 ? 9 : 12); // 파란색 또는 빨간색
     printf("%s색의 턴입니다!\n", turn == 0 ? "파란" : "빨간");
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7); // 기본 색상 복원
 }
